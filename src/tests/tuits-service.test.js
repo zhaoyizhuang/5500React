0a834d278ce882da32e72af30637eba22e128427
ericzzy <ericzhuangzhaoyi@outlook.com>
2022-03-10 23:36:01 -0500

    Tuit test

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -11,13 +11,13 @@ import {
 
 describe('can create tuit with REST API', () => {
     const testUser = {
-        username: 'test',
-        password: 'testWord',
-        email: 'test@test.com'
+        username: 'test1',
+        password: 'testWord1',
+        email: 'test1@test.com'
     };
 
     const testTuit = {
-        "tuit": "test tuit"
+        "tuit": "test1 tuit"
     }
 
     beforeAll(async () => {
@@ -42,7 +42,7 @@ describe('can create tuit with REST API', () => {
         return deleteUsersByUsername(testUser.username);
     })
 
-    test('can insert new tuit with REST API', async () => {
+    test('can create tuit with REST API', async () => {
         // insert new tuit in the database
         const newUser = await createUser(testUser);
         const newTuit = await createTuit(newUser._id, testTuit);
@@ -54,20 +54,20 @@ describe('can create tuit with REST API', () => {
 
 describe('can delete tuit wtih REST API', () => {
     const testUser = {
-        username: 'test',
-        password: 'testWord',
-        email: 'test@test.com'
+        username: 'test2',
+        password: 'testWord2',
+        email: 'test2@test.com'
     };
 
     const testTuit = {
-        "tuit": "test tuit"
+        "tuit": "test2 tuit"
     }
 
     afterAll(async () => {
         return deleteUsersByUsername(testUser.username);
     })
 
-    test('can insert new tuit with REST API', async () => {
+    test('can delete tuit wtih REST API', async () => {
         // delete a tuit by tuit id.
         const newUser = await createUser(testUser);
         const newTuit = await createTuit(newUser._id, testTuit);
@@ -79,9 +79,113 @@ describe('can delete tuit wtih REST API', () => {
 });
 
 describe('can retrieve a tuit by their primary key with REST API', () => {
-  // TODO: implement this
+    const testUser = {
+        username: 'test3',
+        password: 'testWord3',
+        email: 'test3@test.com'
+    };
+
+    const testTuit = {
+        "tuit": "test3 tuit"
+    }
+
+    beforeAll(async () => {
+        //find all user called test
+        const users = await findAllUsers();
+        const testUsers = users.filter(
+            user => testUser.username === user.username);
+        //delete all tuits by their uid
+        for (const u of testUsers) {
+            await deleteTuitsByUser(u._id);
+        }
+        return deleteUsersByUsername(testUser.username);
+    })
+
+    afterAll(async () => {
+        const users = await findAllUsers();
+        const testUsers = users.filter(
+            user => testUser.username === user.username);
+        for (const u of testUsers) {
+            await deleteTuitsByUser(u._id);
+        }
+        return deleteUsersByUsername(testUser.username);
+    })
+
+    test('can retrieve a tuit by their primary key with REST API', async () => {
+        // insert new tuit in the database
+        const newUser = await createUser(testUser);
+        const newTuit = await createTuit(newUser._id, testTuit);
+
+        // verify inserted tuit's properties match parameter tuit
+        expect(newTuit.tuit).toEqual(testTuit.tuit);
+
+        const existingTuit = await findTuitById(newTuit._id);
+
+        expect(newTuit._id).toEqual(existingTuit._id);
+        expect(existingTuit.tuit).toEqual(testTuit.tuit);
+    });
+
 });
 
 describe('can retrieve all tuits with REST API', () => {
-  // TODO: implement this
+
+    // sample users we'll insert to then retrieve
+    const usernames = [
+        "t1", "t2", "t3"
+    ];
+
+    const tuits = [
+        "tuit1", "tuit2"
+    ];
+
+    beforeAll(async () => {
+        for (const u of usernames) {
+            const newU = await createUser(
+                {
+                    username: u,
+                    password: `${u}456`,
+                    email: `${u}@test.com`
+                }
+            );
+            for (const t of tuits) {
+                await createTuit(newU._id, {tuit: t});
+            }
+        }
+    }
+    );
+
+    afterAll(async () => {
+        const users = await findAllUsers();
+        const usersWeInserted = users.filter(
+            user => usernames.indexOf(user.username) >= 0);
+        for (const u of usersWeInserted) {
+            await deleteTuitsByUser(u._id);
+            await deleteUsersByUsername(u.username);
+        }
+    })
+
+    test('can retrieve all tuits with REST API', async () => {
+        const allTuits = await findAllTuits();
+
+        expect(allTuits.length).toBeGreaterThanOrEqual(usernames.length * tuits.length);
+
+        const newUsers = await Promise.all(usernames.map(
+            async name => await findUserByUsername(name)
+        ))
+
+        let count = 0;
+        newUsers.forEach(u => {
+            tuits.forEach( t =>
+                {
+                    if (allTuits.find(
+                        t1 => ((t1.postBy._id === u._id) && (t1.tuit === t))
+                    )) count++;
+                }
+            )
+        }
+        )
+        expect(count).toBeGreaterThanOrEqual(usernames.length * tuits.length);
+
+    })
+
 });
